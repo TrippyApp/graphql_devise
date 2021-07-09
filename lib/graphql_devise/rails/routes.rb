d95e6371c5dd2dab4e120145a4e85c89f72c997f
Osmar Coronel <osmar@Osmars-MacBook-Pro.local>
2021-07-09 16:41:54 -0600

    Add final option for resource loader

--- lib/graphql_devise/rails/routes.rb
+++ lib/graphql_devise/rails/routes.rb
@@ -5,7 +5,8 @@ module ActionDispatch::Routing
     def mount_graphql_devise_for(resource, options = {})
       clean_options = GraphqlDevise::ResourceLoader.new(resource, options, true).call(
         GraphqlDevise::Types::QueryType,
-        GraphqlDevise::Types::MutationType
+        GraphqlDevise::Types::MutationType,
+        nil
       )
 
       post clean_options.at, to: 'graphql_devise/graphql#auth'
--- lib/graphql_devise/version.rb
+++ lib/graphql_devise/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module GraphqlDevise
-  VERSION = '0.17.2'.freeze
+  VERSION = '0.17.3'.freeze
 end
