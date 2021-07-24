25a49b4ba8d2a43638d2f2990522425679de2307
Osmar Coronel <osmar@Osmars-MacBook-Pro.local>
2021-07-24 15:33:21 -0400

    Up a version, remove email  as required

--- lib/graphql_devise/types/authenticatable_type.rb
+++ lib/graphql_devise/types/authenticatable_type.rb
@@ -3,7 +3,7 @@
 module GraphqlDevise
   module Types
     class AuthenticatableType < GraphQL::Schema::Object
-      field :email, String, null: false
+      field :email, String, null: true
     end
   end
 end
--- lib/graphql_devise/version.rb
+++ lib/graphql_devise/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module GraphqlDevise
-  VERSION = '0.17.7'.freeze
+  VERSION = '0.17.8'.freeze
 end
